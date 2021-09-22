83c507d5a914b097794c761274282258202599bb
mbdesigns1989 <mbdesigns1989@yahoo.com>
2021-09-22 16:48:09 +0300

    Add function to Display number of likes for each item

---
+++ src/likesApi.js
@@ -0,0 +1,14 @@
+export const postLikes = async (url, mealId) => {
+    const request = await fetch(url, {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        item_id: mealId,
+      }),
+    });
+    return request;
+  };
+
